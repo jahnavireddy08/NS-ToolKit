ea49cd2572f10fff49fc632bf611f6cc8c5b42c7
jahnavireddy08 <166358114+jahnavireddy08@users.noreply.github.com>
2024-04-08 03:04:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,23 +11,24 @@ def nmap():
     window.resizable(False, False)
     def domain():
         try:
-            ans.insert('end', text="Scanning...\n\n")
+            
+            ans.configure( text="Scanning...\n\n")
             get = ctk.CTkInputDialog(text="Enter a domain: ", title="Nmap Scanner")
             target = socket.gethostbyname(get.get_input())
             out = nmapscan.scan(target)
-            ans.insert('end', text=out + "\n\n")
+            ans.configure( text=out + "\n\n")
         except Exception as e:
-            ans.insert('end', text="Enter a valid domain.\n\n")
+            ans.configure( text="Enter a valid domain.\n\n")
 
     def ipaddr():
         try:
-            ans.insert('end', text="Scanning...\n\n")
+            ans.configure( text="Scanning...\n\n")
             get = ctk.CTkInputDialog(text="Enter an IP: ", title="Nmap Scanner")
             target = get.get_input()
             out = nmapscan.scan(target)
-            ans.insert('end', text=out + "\n\n")
+            ans.configure( text=out + "\n\n")
         except Exception as e:
-            ans.insert('end', text="Enter a valid IP.\n\n")
+            ans.configure( text="Enter a valid IP.\n\n")
 
     dom = ctk.CTkButton(window, text="Enter a domain: ", command=domain)
     dom.place(x=150, y=50, anchor="center")
@@ -43,27 +44,27 @@ def banner_grab():
 
     def domain():
         try:
-            ans.insert('end', text="Grabbing banner...\n\n")
+            ans.configure( text="Grabbing banner...\n\n")
             get0 = ctk.CTkInputDialog(text="Enter a url: ", title="Banner Grabber")
             url = socket.gethostbyname(get0.get_input())
             get1 = ctk.CTkInputDialog(text="Enter a port: ", title="Banner Grabber")
             port = get1.get_input()
             out = banner_grabber.banner_grabber(url, int(port))
-            ans.insert('end', text=out + "\n\n")
+            ans.configure( text=out + "\n\n")
         except Exception as e:
-            ans.insert('end', text="Enter a valid url and port.\n\n")
+            ans.configure( text="Enter a valid url and port.\n\n")
 
     def ipaddr():
         try:
-            ans.insert('end', text="Grabbing banner...\n\n")
+            ans.configure( text="Grabbing banner...\n\n")
             get0 = ctk.CTkInputDialog(text="Enter an IP: ", title="Banner Grabber")
             ip = get0.get_input()
             get1 = ctk.CTkInputDialog(text="Enter a port: ", title="Banner Grabber")
             port = get1.get_input()
             out = banner_grabber.banner_grabber(ip, int(port))
-            ans.insert('end', text=out + "\n\n")
+            ans.configure( text=out + "\n\n")
         except Exception as e:
-            ans.insert('end', text="Enter a valid ip and port.\n\n")
+            ans.configure( text="Enter a valid ip and port.\n\n")
 
     dom = ctk.CTkButton(window, text="Enter a domain: ", command=domain)
     dom.place(x=150, y=100, anchor="center")
@@ -79,14 +80,14 @@ def locate():
 
     def domain():
         try:
-            ans.insert('end', text="Locating...\n\n")
+            ans.configure( text="Locating...\n\n")
             get = ctk.CTkInputDialog(text="Enter a url: ", title="GeoIP")
             url = get.get_input()
             out = geoip.get_loc(url)
-            ans.insert('end', text=out + "\n\n")
+            ans.configure( text=out + "\n\n")
 
         except Exception as e:
-            ans.insert('end', text="Please enter a valid url.\n\n")
+            ans.configure( text="Please enter a valid url.\n\n")
 
 
     dom = ctk.CTkButton(window, text="GeoIP ", command=domain)
@@ -101,11 +102,11 @@ def wifiscan():
 
     def domain():
         try:
-            ans.insert('end', text="Scanning...\n\n")
+            ans.configure( text="Scanning...\n\n")
             out = wifiscanner.wifi(range.get())
-            ans.insert('end', text=out + "\n\n")
+            ans.configure( text=out + "\n\n")
         except Exception as e:
-            ans.insert('end', text="Please enter a valid ip range.\n\n")
+            ans.configure( text="Please enter a valid ip range.\n\n")
 
     iplabel = ctk.CTkLabel(window, text="Enter a ip range: ")
     iplabel.place(x=150, y=50, anchor="center")
@@ -123,11 +124,11 @@ def sniff():
 
     def domain():
         try:
-            ans.insert('end', "Sniffing...\n\n")
+            ans.configure( "Sniffing...\n\n")
             out = packet_sniff.packet(int(count.get()))
-            ans.insert('end', text=out + "\n\n")
+            ans.configure( text=out + "\n\n")
         except Exception as e:
-            ans.insert('end', text="Please enter a valid number.\n\n")
+            ans.configure( text="Please enter a valid number.\n\n")
 
     countlabel = ctk.CTkLabel(window, text="Enter the number of packets to be sniffed: ")
     countlabel.place(x=150, y=50, anchor="center")
@@ -137,39 +138,41 @@ def sniff():
     dom.place(x=150, y=150, anchor="center")
     
 ctk.set_appearance_mode("Light")
-#ctk.set_default_color_theme("Night_Purple.json")
+ctk.set_default_color_theme("coffee.json")
 
 app = ctk.CTk()
-app.geometry("720x720")
+app.geometry("720x600")
 app.resizable(False, False)
+
 
 app.title("NS Toolkit")
 
-title = ctk.CTkLabel(app, text="Choose an Option: ", font=("Baskerville Old Face", 25))
-title.place(x=350, y=100, anchor="center")
+title = ctk.CTkLabel(app, text="Select an Option: ", font=("Baskerville Old Face", 25))
+title.place(x=359, y=100, anchor="center")
 
-nmaps = ctk.CTkButton(app, text="Nmap Scan", command=nmap , fg_color= 'Blue')
-nmaps.place(x=350, y=150, anchor="center")
+nmaps = ctk.CTkButton(app, text="Nmap Scan", command=nmap )
+nmaps.place(x=250, y=150, anchor="center")
 
-ban = ctk.CTkButton(app, text="Banner Grabber", command=banner_grab ,fg_color= 'Yellow',text_color='Black')
-ban.place(x=350, y=200, anchor="center")
+ban = ctk.CTkButton(app, text="Banner Grabber", command=banner_grab )
+ban.place(x=250, y=200, anchor="center")
 
-loc = ctk.CTkButton(app, text="Geolocation", command=locate,fg_color= 'Green')
-loc.place(x=350, y=250, anchor="center")
+loc = ctk.CTkButton(app, text="Geolocation", command=locate)
+loc.place(x=480, y=150, anchor="center")
 
 wifi = ctk.CTkButton(app, text="Wifi Scanner", command=wifiscan)
-wifi.place(x=350, y=300, anchor="center")
+wifi.place(x=480, y=200, anchor="center")
 
-pack = ctk.CTkButton(app, text="Packet Sniffer", command=sniff , fg_color= 'Blue')
-pack.place(x=350, y=350, anchor="center")
+pack = ctk.CTkButton(app, text="Packet Sniffer", command=sniff )
+pack.place(x=365, y=250, anchor="center")
 
-output = ctk.CTkLabel(app, text="Output: ", font=("Century Gothic", 15))
-output.place(x=350, y=400, anchor="center")
+output = ctk.CTkLabel(app, text="Output: ", font=("Century Gothic", 20))
+output.place(x=365, y=340, anchor="center")
 
-ans = ctk.CTkTextbox(app, width=500, height=200, )
-ans.place(x=350, y=475, anchor="center")
 
-quit = ctk.CTkButton(app, text="Quit Toolkit", command=app.quit , fg_color= 'Red')
-quit.place(x=350, y=650, anchor="center")
+ans = ctk.CTkLabel(app, text="", width=500, height=200, )
+ans.place(x=365, y=460, anchor="center")
+
+quit = ctk.CTkButton(app, text="Quit Toolkit", command=app.quit)
+quit.place(x=365, y=550, anchor="center")
 
 app.mainloop()
