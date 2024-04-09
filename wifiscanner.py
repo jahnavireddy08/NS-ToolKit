e465312f5a5fa15ca525b0c3c2b63bbdd6018199
jahnavireddy08 <166358114+jahnavireddy08@users.noreply.github.com>
2024-04-09 02:10:51 -0400

    Update wifiscanner.py

--- wifiscanner.py
+++ wifiscanner.py
@@ -1,17 +1,30 @@
 import scapy.all as scapy
-import re
 import socket
 
-def wifi(ip_add_range_entered):
+def wifi_scan(ip_range):
+    results = []
 
-    arp_result = scapy.arping(ip_add_range_entered, verbose=False)[0]
+    # Send ARP request to the specified IP range
+    arp_request = scapy.ARP(pdst=ip_range)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-    for sent, received in arp_result:
-        ip = received.psrc
-        mac = received.hwsrc
+    # Process the responses
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
         try:
-            hostname, _, _ = socket.gethostbyaddr(ip)
+            client_dict["hostname"] = socket.gethostbyaddr(element[1].psrc)[0]
         except socket.herror:
-            hostname = "Unknown"
+            client_dict["hostname"] = "Unknown"
+        results.append(client_dict)
 
-        return f"IP: {ip} MAC: {mac} Hostname: {hostname}"
+    return results
+
+if __name__ == "__main__":
+    ip_range = input("Enter the IP range to scan (e.g., 192.168.1.0/24): ")
+    scan_results = wifi_scan(ip_range)
+    print("IP\t\t\tMAC Address\t\t\tHostname")
+    print("-" * 60)
+    for client in scan_results:
+        print(f"{client['ip']}\t{client['mac']}\t{client['hostname']}")
